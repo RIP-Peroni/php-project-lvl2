ac89933282613cc469a711395d27405a97da7c62
Yuriy <epih96@mail.ru>
2021-09-07 23:54:33 +0300

    1st step logic is added, fixtures for usage

---
+++ src/Differ.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Differ\Differ;
+
+function parseJson(string $content): object
+{
+    return json_decode($content);
+}
+
+function generateDiff(string $pathToFile1, string $pathToFile2): string
+{
+    $structure1 = parseJson(file_get_contents($pathToFile1));
+    $structure2 = parseJson(file_get_contents($pathToFile2));
+    $diffTree = getDiffTree($structure1, $structure2);
+    return format($diffTree);
+}
+
+function getDiffTree(object $structure1, object $structure2): array
+{
+    $keys = array_keys(array_merge((array) $structure1, (array) $structure2));
+    sort($keys);
+    return array_map(
+        function ($key) use ($structure1, $structure2) {
+            $oldValue = $structure1->$key ?? null;
+            $newValue = $structure2->$key ?? null;
+
+            if (!property_exists($structure2, $key)) {
+                $type = 'removed';
+            } elseif (!property_exists($structure1, $key)) {
+                $type = 'added';
+            } elseif ($oldValue !== $newValue) {
+                $type = 'modified';
+            } else {
+                $type = 'unmodified';
+            }
+
+            return [
+                'key' => $key,
+                'type' => $type,
+                'oldValue' => $oldValue,
+                'newValue' => $newValue
+            ];
+        },
+        $keys
+    );
+}
+
+function format(array $diffTree): string
+{
+    $result = stylize($diffTree);
+    return "\n" . "{$result}" . "\n";
+}
+
+function stylize(array $diffTree): string
+{
+    $result = array_map(
+        function ($node) {
+            $type = $node['type'];
+            $key = $node['key'];
+            $bigIndent = str_repeat(' ', 4);
+            $smallIndent = str_repeat(' ', 2);
+
+            switch ($type) {
+                case 'unmodified':
+                    $value = $node['oldValue'];
+                    return "{$bigIndent}{$key}: {$value}";
+                case 'modified':
+                    $oldValue = $node['oldValue'];
+                    $newValue = $node['newValue'];
+                    return "{$smallIndent}- {$key}: {$oldValue}\n"
+                        . "{$smallIndent}+ {$key}: {$newValue}";
+                case 'added':
+                    $value = $node['newValue'];
+                    return "{$smallIndent}+ {$key}: {$value}";
+                case 'removed':
+                    $value = $node['oldValue'];
+                    return "{$smallIndent}- {$key}: {$value}";
+                default:
+                    throw new \Exception('Wrong node type');
+            }
+        },
+        $diffTree
+    );
+    return implode("\n", $result);
+}
